cb7e7d75c9b815c6c2cf68d3122b557385523d84
Ravi-Sinhmar <engineerfuture82@gmail.com>
2025-02-23 18:51:12 +0530

    Updated websokets url also for production

--- client/src/utils/websocket.js
+++ client/src/utils/websocket.js
@@ -1,5 +1,11 @@
+const BACKEND_URL =
+import.meta.env.VITE_ENV === "Production"
+  ? import.meta.env.VITE_PRODUCTION_BACKEND_URL
+  : import.meta.env.VITE_LOCAL_BACKEND_URL;
+
+
 import { io } from "socket.io-client";
-const SOCKET_SERVER_URL = "http://localhost:5000"; // Replace with your backend URL
+const SOCKET_SERVER_URL = BACKEND_URL; // Replace with your backend URL
 export const socket = io(SOCKET_SERVER_URL, {
   autoConnect: false,  // Prevents auto connection, you control it manually
   transports: ["websocket"], // Ensures stable connection
